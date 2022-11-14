2c05cf82285c741aaf077c7ba4032179e5ed1623
tecnicotronfullstack@gmail.com <tecnicotronfullstack@gmail.com>
2022-11-14 15:50:38 -0300

    :recycle: refactor: Replacing get user() method with API get_users() and added checking for blocking in block function

--- blocklist.py
+++ blocklist.py
@@ -1,3 +1,5 @@
+#True para desbloquear usuários da lista
+unblock = False
 #Username de cada usuário que será bloqueado
 usernames = [
     "USERNAME",
--- main.py
+++ main.py
@@ -1,26 +1,35 @@
 import tweepy
 from credentials import *
-from blocklist import usernames
-#Criando um Cliente para a API 
-api = tweepy.Client(
-    consumer_key=consumer_key,
-    consumer_secret=consumer_secret,
-    access_token= access_token ,
-    access_token_secret=access_token_secret)
-try:
-    #Percorre o blocklist      
-    for username in usernames:
-        #Pesquisa o usuário
-        user = api.get_user(username=username, user_auth=True)   
-        #Pega o ID do usuário
-        user_id = user[0].data["id"]
-        #Bloqueia o usuário  	
-        res = api.block(target_user_id=user_id)       
-        is_blocked = res[0]["blocking"]
-        #Confirmação
-        if is_blocked:
-            print(f"@{username} está bloquedo")
-        else:
-            print(f"@{username} não está bloquedo")		
-except Exception as erro:
-    print("Erro:", erro)
+from blocklist import usernames,unblock
+def block(api, username,unblock=False):
+    try:
+        #Busca os usuarios pela lista de usernames
+        users = api.get_users(usernames=usernames, user_auth=True)[0]
+        for user in users:
+            user_id = user.data["id"]
+            username = user.data["username"]
+            target_user_id = user_id
+            #Verifica bloqueio
+            if unblock == False:           
+                res = api.block(target_user_id) 
+            else: 
+                res = api.unblock(target_user_id)     
+            is_blocked = res[0]["blocking"]
+            #Confirmação
+            if is_blocked:
+                print(f"@{username} está bloquedo")
+            else:
+                print(f"@{username} não está mais bloquedo")		
+    except Exception as erro:
+        print("Erro:", erro)
+
+if __name__ == "__main__":
+    #Criando um Cliente para a API 
+    api = tweepy.Client(
+        consumer_key=consumer_key,
+        consumer_secret=consumer_secret,
+        access_token= access_token ,
+        access_token_secret=access_token_secret)
+    #Bloqueia ou desbloqueia usuários da lista
+    block(api, usernames, unblock=unblock)
+
