3924e106ac7c86c71ec0aba0b7896bcdae5ba456
tecnicotronfullstack@gmail.com <tecnicotronfullstack@gmail.com>
2022-11-14 21:47:53 -0300

    :books: docs: Added Env For Credential Detachment

--- credentials.py
+++ credentials.py
@@ -1,5 +1,9 @@
-# Atribua as suas credenciais
-consumer_key = "CHAVE DE ACESSO" 
-consumer_secret = "CHAVE SECRETA DE ACESSO" 
-access_token = "TOKEN DE ACESSO"
-access_token_secret = "TOKEN SECRETO DE ACESSO"
+from decouple import config
+consumer_key = config("CONSUMER_KEY")
+consumer_secret = config("CONSUMER_SECRET")
+access_token = config("ACCESS_TOKEN")
+    
+access_token_secret = config("ACCESS_TOKEN_SECRET")
+
+
+
