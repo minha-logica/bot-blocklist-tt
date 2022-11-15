cb8285706ab90f4c90722834bc41cac7af6840a4
tecnicotronfullstack@gmail.com <tecnicotronfullstack@gmail.com>
2022-11-15 00:26:48 -0300

    :zap: perf: Optimizing block function

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 import tweepy
 
-def block(api, username,unblock=False):
+def block(api, usernames,unblock=False):
     try:
         #Busca os usuarios pela lista de usernames
         users = api.get_users(usernames=usernames, user_auth=True)[0]
+        #Verifica bloqueio ou desbloqueio
+        func_block = api.unblock if unblock else api.block
         for user in users:
             user_id = user.data["id"]
             username = user.data["username"]
-            target_user_id = user_id
-            #Verifica bloqueio
-            if unblock == False:           
-                res = api.block(target_user_id) 
-            else: 
-                res = api.unblock(target_user_id)     
+            res = func_block(target_user_id = user_id)
             is_blocked = res[0]["blocking"]
             #Confirmação
             if is_blocked:
@@ -33,4 +30,3 @@ if __name__ == "__main__":
         access_token_secret=access_token_secret)
     #Bloqueia ou desbloqueia usuários da lista
     block(api, usernames, unblock=unblock)
-
