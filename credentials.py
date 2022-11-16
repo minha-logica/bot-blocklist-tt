dd7b3d46db6872bb7a96df625f8feff0929d278f
tecnicotronfullstack@gmail.com <tecnicotronfullstack@gmail.com>
2022-11-16 04:06:50 -0300

    :fire: Removing credentials.py file

--- credentials.py
+++
@@ -1,9 +0,0 @@
-from decouple import config
-consumer_key = config("CONSUMER_KEY")
-consumer_secret = config("CONSUMER_SECRET")
-access_token = config("ACCESS_TOKEN")
-    
-access_token_secret = config("ACCESS_TOKEN_SECRET")
-
-
-
