0d65676fe1c6db529dbfef2e0bf55aa49d46b958
tecnicotronfullstack@gmail.com <tecnicotronfullstack@gmail.com>
2022-11-20 00:59:18 -0300

    :white_check_mark: test: Update test_blocklist

--- test_blocklist.py
+++ test_blocklist.py
@@ -2,19 +2,19 @@ from blocklist import BlockList
 import unittest
 
 class BlockListTests(unittest.TestCase):
-    def test_block(self):
-        blocklist = BlockList(file_path="tests/usernames.txt")
-        usernames = blocklist.read()
-        blocked = blocklist.block(usernames, unblock=True)        
-        self.assertEqual(True, blocked)
-    def test_search_in_followers(self):       
+    
+     def test_search_in_followers(self):       
         blocklist = BlockList(
             	           filters=[
             	               "chatonildo",
             	           ]
             	       )           
         results = blocklist.find_followers() 
-        returned = True if len(results) > 0 else False  
-                                                
+        returned = True if len(results) > 0 else False                                                  
         self.assertEqual(True, returned)
-
+        
+    def test_block(self):
+        blocklist = BlockList(file_path="tests/usernames.txt")
+        usernames = blocklist.read()
+        blocked = blocklist.block(usernames, unblock=True)        
+        self.assertEqual(True, blocked)
